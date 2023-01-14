46e7c49ee818c9ec1e8efd4ec5784e680cad49c4
ArtemGladkov <2020artem@mail.ru>
2023-01-14 20:59:08 +0300

    DZ3_GladkovArtem_REST

--- librari/mainapp/serializers.py
+++ librari/mainapp/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
+from .models import Author, Project, TODO
 
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Author
+        fields = '__all__'
+
+
+class ProjectModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class TODOModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = TODO
         fields = '__all__'
